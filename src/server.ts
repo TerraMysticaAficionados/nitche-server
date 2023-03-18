142b9d2d58c13a481b9663e66ed227a10f293641
nick <nicholasdario11@gmail.com>
2023-03-18 15:41:49 -0700

    feat: brittle but simple front-end + back-end in typescript

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express"
+import {join} from "path"
+import browserify from "browserify-middleware" 
+
 const app = express()
 const port = 8080
 
+app.use('/index.js', browserify("./dist/app/index.js"))
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(join(__dirname,"../src/app/index.html"))
 })
 
 app.listen(port, () => {
---
+++ types/browserify-middleware.d.ts
@@ -0,0 +1,29 @@
+declare module "browserify-middleware" {
+    import type Express from "express"
+    declare type Arraified<T> = T[]|boolean
+    declare const BrowserifyMiddlewareSettings: Partial<{
+        external: Arraified<unknown>
+        ignore: Arraified<unknown>
+        ignoreMissing: boolean
+        transform: Arraified<unknown>
+        insertGlobals: boolean
+        detectGlobals: boolean
+        standalone: boolean
+        noParse: Arraified<unknown>
+        extensions: Arraified<unknown>
+        basedir: string
+        grep: RegExp
+        cache: string|boolean|object
+        minify: boolean
+        gzip: boolean
+        debug: boolean
+        precompile: boolean
+    }>
+    
+    declare function browserify(path: string, options?:BrowserifyMiddlewareSettings): Express.NextFunction
+    export function directory(path: string, options?:BrowserifyMiddlewareSettings): Express.NextFunction
+    export function file(path: string, options?:BrowserifyMiddlewareSettings): Express.NextFunction
+    export function modules(path: string, options?:BrowserifyMiddlewareSettings): Express.NextFunction
+    export const settings: BrowserifyMiddlewareSettings
+    export default browserify
+}
